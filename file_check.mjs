52ebb089d3ea8f2a91b2e77053bed2de705f9bde
VirtualPirate <artaza13020artaza@gmail.com>
2023-02-12 20:01:45 +0530

    Removed file_check.mjs

--- file_check.mjs
+++
@@ -1,5 +0,0 @@
-import fs from "fs";
-
-console.log(fs.existsSync("Hellow.txt"));
-console.log(fs.existsSync("https!nextjs.org.json"));
-console.log(fs.existsSync("./public/ogData/https!nextjs.org.json"));
